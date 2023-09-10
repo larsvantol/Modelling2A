8efa500314161f7b0f5f7915bf0e4689934bf3a6
larsvantol <LMJvanTol@gmail.com>
2023-09-10 10:44:33 +0200

    improved simulation

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from matplotlib.animation import FuncAnimation
 from tqdm import tqdm
 from colors import random_color
 import time
+from scipy.stats import poisson, norm, expon, lognorm
+from scipy.optimize import curve_fit
+import os
 
 
 class Road:
@@ -17,16 +20,23 @@ class Road:
         self.lanes = {i: [] for i in range(num_of_lanes)}
         self.lane_width = 5  # m
 
+        self.data = []
+
     def add_car(self, car, lane_index) -> None:
         # Adds a car to the road on the lane given the lane_index
         self.lanes[lane_index].append(car)
-        print(f"Car {car.name} has entered the road on lane {lane_index}")
+        # print(f"Car {car.name} has entered the road on lane {lane_index}")
 
     def remove_car(self, car, lane_index) -> None:
         # Removes a car from the road on the lane given the lane_index
         if car in self.lanes[lane_index]:
             self.lanes[lane_index].remove(car)
-            print(f"Car {car.name} has left the road")
+            # print(f"Car {car.name} has left the road")
+            # If the car is removed from the road, add the travel time to the data check if the car is not on x=0
+            if car.x != 0:
+                self.data.append(sim_time - car.start_time)
+                # print(f"Travel time of {car.name}: {sim_time - car.start_time:.2f} s")
+                # print(f"Average travel time: {np.mean(self.data):.2f} s")
 
     def move_vehicle_to_lane(self, vehicle, from_lane, to_lane):
         if from_lane != to_lane:
@@ -103,6 +113,17 @@ class Car:
         self.x = 0
         self.speed = self.initial_normal_speed()
 
+        self.save_space_time = np.random.default_rng().normal(
+            2, 0.5
+        )  # s (2 second rule)
+
+        self.start_time = sim_time
+        self.logging = False
+
+    def print_log(self, log) -> None:
+        if self.logging:
+            print(log)
+
     def move(self, delta_t, road, lane_index) -> None:
         lanes = road.lanes
         # Check if the car can move back to a previous lane
@@ -129,12 +150,12 @@ class Car:
                 return
 
     def maintain_speed(self, delta_t) -> None:
-        print(f"{self.name} is maintaining speed")
+        self.print_log(f"{self.name} is maintaining speed")
         self.x += delta_t * self.speed
         self.speed = self.calculate_new_speed()
 
     def should_overtake(self, road, lane_index) -> bool:
-        print(f"{self.name} is checking if it should overtake")
+        self.print_log(f"{self.name} is checking if it should overtake")
         # Check if it is possible to move to a higher lane
         if lane_index == road.num_of_lanes - 1:
             return False
@@ -152,14 +173,14 @@ class Car:
             road.lanes[lane_index + 1]
         )
         if next_lane_save_front and next_lane_save_behind:
-            print(f"{self.name} should overtake {car_blocking.name}")
+            self.print_log(f"{self.name} should overtake {car_blocking.name}")
             return True
 
         else:
             return False
 
     def overtake(self, road, lane_index) -> None:
-        print(f"{self.name} is overtaking")
+        self.print_log(f"{self.name} is overtaking")
         # First check if the car can move to a higher lane (does not check if the lane is save)
         if lane_index == road.num_of_lanes - 1:
             pass
@@ -167,7 +188,7 @@ class Car:
         road.move_vehicle_to_lane(self, lane_index, lane_index + 1)
 
     def should_move_back(self, road, lane_index) -> bool:
-        print(f"{self.name} is checking if it should move back")
+        self.print_log(f"{self.name} is checking if it should move back")
         # Check if it is possible to move to a previous lane
         if lane_index == 0:
             return False
@@ -181,7 +202,7 @@ class Car:
             previous_car_blocking_behind,
         ) = self.is_lane_save_behind(road.lanes[lane_index - 1])
         if previous_lane_save_front and previous_lane_save_behind:
-            print(f"{self.name} should move back")
+            self.print_log(f"{self.name} should move back")
             return True
         else:
             previous_car_blocking = (
@@ -189,13 +210,13 @@ class Car:
                 if previous_car_blocking_front is not None
                 else previous_car_blocking_behind
             )
-            print(
+            self.print_log(
                 f"{self.name} should not move back due to {previous_car_blocking.name}"
             )
             return False
 
     def move_back(self, road, lane_index) -> None:
-        print(f"{self.name} is moving back")
+        self.print_log(f"{self.name} is moving back")
         # First check if the car can move to a previous lane (does not check if the lane is save)
         if lane_index == 0:
             pass
@@ -205,30 +226,30 @@ class Car:
     def is_lane_save_in_front(self, lane) -> tuple[bool, Self or None]:
         # Checks if the lane is save for the car to drive on
         # Distance according to the 2 second rule
-        save_distance = 1 * self.speed
+        save_distance = self.save_space_time * self.speed
         for car in lane:
             if car == self:
                 break
             if car.x < self.x + save_distance and car.x > self.x:
-                print(
+                self.print_log(
                     f"The lane is not save for {self.name} due to the following car: {car.name}"
                 )
-                print(f"Distance: {car.x - self.x:.2f} m")
+                self.print_log(f"Distance: {car.x - self.x:.2f} m")
                 return (False, car)
         return (True, None)
 
     def is_lane_save_behind(self, lane) -> tuple[bool, Self or None]:
         # Checks if the lane is save for the car to drive on
         # Distance according to the 2 second rule
-        save_distance = 1 * self.speed
+        save_distance = self.save_space_time * self.speed
         for car in lane:
             if car == self:
                 break
             if car.x > self.x - save_distance and car.x < self.x:
-                print(
+                self.print_log(
                     f"The lane is not save for {self.name} due to the following car: {car.name}"
                 )
-                print(f"Distance: {self.x - car.x:.2f} m")
+                self.print_log(f"Distance: {self.x - car.x:.2f} m")
                 return (False, car)
         return (True, None)
 
@@ -313,65 +334,223 @@ def spawn_car_on_first_lane(road):
             road.remove_car(car, lane_index=0)
 
 
-def main() -> None:
-    results = np.array([])
-    delta_t = 0.1  # s
+def simulate(road, delta_t):
+    global sim_time  # Declare sim_time as nonlocal to modify it within the function
+    sim_time += delta_t  # Update simulation time as the simulation progresses
 
-    road1 = Road(length=200, num_of_lanes=3)
+    spawn_car_on_first_lane(road)
+    if road.cars_on_road():
+        road.move_cars(delta_t=delta_t)
 
-    fig = plt.figure()
-    ax = plt.axes(
-        xlim=(0, road1.length), ylim=(0, road1.num_of_lanes * road1.lane_width)
+
+def get_stats(road, simulation_time_data):
+    stats = dict()
+    stats["min_travel_time"] = np.min(road.data)
+    stats["max_travel_time"] = np.max(road.data)
+    stats["mean_travel_time"] = np.mean(road.data)
+    stats["std_dev"] = np.std(road.data)
+
+    bins = np.arange(
+        stats["min_travel_time"],
+        stats["max_travel_time"],
+        (stats["max_travel_time"] - stats["min_travel_time"]) / 50,
     )
+    hist, bins = np.histogram(road.data, bins=bins)
+
+    stats["most_common_travel_time"] = bins[np.argmax(hist)]
+    stats["amount_of_most_common_travel_time"] = np.argmax(road.data)
+    stats["real_time"] = simulation_time_data["real_time"]
+    stats["simulation_time"] = simulation_time_data["simulation_time"]
+    stats["amount_of_cars"] = len(road.data)
+    stats["road_length"] = road.length
+    stats["num_of_lanes"] = road.num_of_lanes
+    stats["bins"] = bins
+    return stats
+
+
+def print_stats(stats):
+    # Print stats using the stats dictionary make sure they are aligned
+    print(f"Road length: \t\t{stats['road_length']:.2f} m")
+    print(f"Number of lanes: \t{stats['num_of_lanes']}")
+    print(
+        f"Minimum travel time: \t{stats['min_travel_time']:.2f} s or {stats['min_travel_time'] / 60:.2f} min"
+    )
+    print(
+        f"Maximum travel time: \t{stats['max_travel_time']:.2f} s or {stats['max_travel_time'] / 60:.2f} min"
+    )
+    print(
+        f"Average travel time: \t{stats['mean_travel_time']:.2f} s or {stats['mean_travel_time'] / 60:.2f} min"
+    )
+    print(
+        f"Most common time: \t{stats['most_common_travel_time']:.2f} s or {stats['most_common_travel_time'] / 60:.2f} min"
+    )
+    print(
+        f"Amount most common time: \t{stats['amount_of_most_common_travel_time']} / {stats['amount_of_cars']}"
+    )
+    print(f"Standard deviation: \t{stats['std_dev']:.2f} s")
+    print(f"Amount of cars: \t{stats['amount_of_cars']}")
+    print(
+        f"Simulation time: \t{stats['simulation_time']:.2f} s or {stats['simulation_time'] / 60:.2f} min"
+    )
+    print(f"Time to simulate: \t{stats['real_time']:.2f} s")
+
+
+def plot_travel_times(road, stats):
+    # Plot the data of the simulation
+
+    plt.figure(figsize=(10, 6))
+
+    plt.hist(
+        road.data,
+        bins=stats["bins"],
+        density=True,
+        label="Simulation data",
+        alpha=0.5,
+        rwidth=0.9,
+        color="b",
+    )
+    plt.title("Travel times")
+
+    # Also plot a line for the average travel time
+    plt.axvline(stats["mean_travel_time"], color="k", linewidth=1)
+    plt.text(
+        stats["mean_travel_time"] + 0.1,
+        0.8 * plt.ylim()[1],
+        f"Average travel time: {stats['mean_travel_time']:.2f} s",
+    )
+
+    # Also plot a line for the most common travel time
+    plt.axvline(stats["most_common_travel_time"], color="k", linewidth=1)
+    plt.text(
+        stats["most_common_travel_time"] + 0.1,
+        0.7 * plt.ylim()[1],
+        f"Most common travel time: {stats['most_common_travel_time']:.2f} s",
+    )
+
+    # Also plot a line for the standard deviation
+    plt.axvline(
+        stats["mean_travel_time"] + stats["std_dev"],
+        color="k",
+        linestyle="dashed",
+        linewidth=1,
+    )
+    plt.text(
+        stats["mean_travel_time"] + stats["std_dev"] + 0.1,
+        0.6 * plt.ylim()[1],
+        f"Standard deviation: {stats['std_dev']:.2f} s",
+    )
+    plt.axvline(
+        stats["mean_travel_time"] - stats["std_dev"],
+        color="k",
+        linestyle="dashed",
+        linewidth=1,
+    )
+
+    # Fit some distributions to the data
+    x = np.linspace(0, stats["max_travel_time"], 1000)
+
+    # Fit the normal distribution to the histogram
+    p = norm.pdf(x, loc=stats["mean_travel_time"], scale=stats["std_dev"])
+    plt.plot(x, p, "g", lw=2, label="Fitted Normal Distribution")
+
+    # Fit a poisson distribution to the data
+    p = poisson.pmf(x, stats["mean_travel_time"])
+    plt.plot(x, p, "b", lw=2, label="Fitted Poisson Distribution")
+
+    # Fit a exponential distribution to the data
+    loc, scale = expon.fit(road.data)
+    p = expon.pdf(x, loc, scale)
+    plt.plot(x, p, "r", label="Fitted Exponential")
+
+    # Fit a exponential distribution to the data for the most common travel time
+    filtered_data = list(
+        filter(lambda time: time >= stats["most_common_travel_time"], road.data)
+    )
+    loc, scale = expon.fit(filtered_data)
+    p = expon.pdf(x, loc, scale)
+    plt.plot(x, p, "r", linestyle="dashed", label="Fitted Exponential")
+
+    # Fit a lognormal distribution to the data
+    shape, loc, scale = lognorm.fit(road.data)
+    p = lognorm.pdf(x, shape, loc, scale)
+    plt.plot(x, p, "y", label="Fitted Lognormal")
+
+    plt.xlabel("Travel time (s)")
+    plt.ylabel("Probability density")
+
+    plt.legend()
+
+    plt.show()
+
+
+def animate(road, delta_t):
+    fig = plt.figure()
+    ax = plt.axes(xlim=(0, road.length), ylim=(0, road.num_of_lanes * road.lane_width))
     ax.set_aspect("equal")
     ax.set_xlabel("Distance (m)")
     ax.set_ylabel("Lane")
     ax.set_title("Traffic simulation")
 
-    # what can be done to make the animation faster?
-    # https://stackoverflow.com/questions/40126176/fast-live-plotting-in-matplotlib-pyplot
-
     def update(frame_number):
         fig.clear()
-        spawn_car_on_first_lane(road1)
-        if road1.cars_on_road():
+
+        global sim_time  # Declare sim_time as nonlocal to modify it within the function
+
+        sim_time += delta_t  # Update simulation time as the simulation progresses
+
+        spawn_car_on_first_lane(road)
+        if road.cars_on_road():
             t1 = time.perf_counter_ns()
-            road1.move_cars(delta_t=delta_t)
+            road.move_cars(delta_t=delta_t)
             t2 = time.perf_counter_ns()
-            print(f"Time to move cars: {t2} - {t1} = {(t2 - t1) / 1e6:.2f} ms")
-            road1.show_graphical_representation()
-            plt.draw()
+            # print(f"Time to move cars: {t2} - {t1} = {(t2 - t1) / 1e6:.2f} ms")
+        # print("frame_number:", frame_number)
+        road.show_graphical_representation()
+        # Add time to the plot
+        plt.text(
+            0.8 * plt.xlim()[1],
+            0.9 * plt.ylim()[1],
+            f"Time: {sim_time:.2f} s",
+            fontsize=12,
+        )
+        # plt.draw()
 
     # Construct the animation, using the update function as the animation director.
-    anim = FuncAnimation(fig, update, 10, interval=5)
-    # anim.save("tmp/animation.mp4", writer="PillowWriter", fps=1)
-    plt.show()
+    anim = FuncAnimation(fig=fig, func=update, frames=400, interval=30)
+    filepath = os.path.abspath("tmp/animation.html")
+    anim.save(filename=filepath, writer="html")
 
-    # for i in tqdm(range(1000)):
-    #     t = 0  # s
-    #     delta_t = 1  # s
+    # open the file
+    os.startfile(filepath)
 
-    #     road1 = Road(length=5000, num_of_lanes=1)
-    #     car1 = Car()
-    #     car2 = Car()
-    #     road1.add_car(car1, lane_index=0)
-    #     road1.add_car(car2, lane_index=0)
 
-    #     for car in road1.lanes[0]:
-    #         while car.x < road1.length:
-    #             t += delta_t
-    #             car.move(delta_t=delta_t)
+def run_simulation(road, delta_t):
+    start_time_of_simulation = time.perf_counter_ns()
 
-    #     results = np.append(results, t)
+    for _ in tqdm(range(100000)):
+        simulate(road, delta_t)
 
-    # min = np.min(results)
-    # print(f"Minimum travel time: {min:.2f} s or {min / 60:.2f} min")
-    # max = np.max(results)
-    # print(f"Maximum travel time: {max:.2f} s or {max / 60:.2f} min")
-    # bins = np.arange(min, max, 5)
-    # plt.hist(results, bins=bins)
-    # plt.title("Travel times")
-    # plt.show()
+    end_time_of_simulation = time.perf_counter_ns()
+    simulation_time_data = {
+        "real_time": (end_time_of_simulation - start_time_of_simulation) / 1e9,
+        "simulation_time": sim_time,
+    }
+
+    stats = get_stats(road, simulation_time_data)
+    print_stats(stats)
+    plot_travel_times(road, stats)
+
+
+def main() -> None:
+    delta_t = 0.1  # s
+
+    global sim_time  # Declare sim_time as a global variable
+    sim_time = 0  # Initialize simulation time
+
+    road1 = Road(length=500, num_of_lanes=3)
+
+    # animate(road1, delta_t)
+    run_simulation(road1, delta_t)
 
 
 if __name__ == "__main__":
