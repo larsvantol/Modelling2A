ba378958734a20b73822911e088fa318f607e170
larsvantol <LMJvanTol@gmail.com>
2023-09-27 09:48:42 +0200

    added gipps safe_speed

--- Vehicle.py
+++ Vehicle.py
@@ -46,15 +46,39 @@ class SimpleBehavior(Behavior):
         if vehicle.velocity < 0:
             vehicle.velocity = 0
 
+
 class Gipps(Behavior):
-    def __init__(self, maximum_acceleration: float, maximum_deceleration: float, desired_velocity: float, apparent_reaction_time: float) -> None:
+    def __init__(
+        self,
+        maximum_acceleration: float,
+        maximum_deceleration: float,
+        desired_velocity: float,
+        apparent_reaction_time: float,
+    ) -> None:
         self.maximum_acceleration = maximum_acceleration
         self.maximum_deceleration = maximum_deceleration
         self.desired_velocity = desired_velocity
         self.apparent_reaction_time = apparent_reaction_time
+        self.comfortable_distance = 2  # m
 
     def set_initial_velocity(self, vehicle: Vehicle):
+        ...
 
+    def update(self, vehicle: Vehicle, lead_vehicle: Vehicle, delta_t: float):
+        # min [v + aΔt, v0, vsafe(s, vl )] gipps equation
+        pass
+
+    def calculate_safe_speed(
+        self, leading_distance: float, leading_velocity: float, delta_t: float
+    ) -> float:
+        # −bT+ sqrt(b^2T^2+2b(s−s0)+vl^2)
+        return self.maximum_acceleration * delta_t + (
+            (self.maximum_acceleration**2) * (delta_t**2)
+            + 2
+            * self.maximum_acceleration
+            * (leading_distance - self.comfortable_distance)
+            + leading_velocity**2
+        ) ** (0.5)
 
 
 class Car(Vehicle):
